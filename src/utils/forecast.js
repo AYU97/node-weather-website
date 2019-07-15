601190d1ba34b523f07828e0c83667dae481091f
Ayush Singh Gaur <ayusin27@in.ibm.com>
2019-07-15 14:10:59 +0530

    Init Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,28 @@
+const request = require('request');
+
+const forecast = (longitude, latitude, callback) => {
+
+    const forcasturl = 'https://api.darksky.net/forecast/650b2d28b15bdc971940b428fc499299/37.8267,-122.4233?';
+
+    request({ url: forcasturl, json: true }, (error, { body}) => {
+
+        if (error) {
+            callback('Unable to connect to the weather app! Check connection.', undefined);
+        } else if (error) {
+            callback('Unable to find location, try another search', undefined);
+        } else {
+            callback(undefined, 
+
+          body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out . There is a ' + body.currently.precipProbability + '% chance of rain. '
+               
+                
+
+
+            )
+        }
+    })
+
+
+}
+
+module.exports = forecast;
